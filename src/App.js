ee42c1ab86ffb38229e633d3edb42e0c0bdb1ebb
Chaman Yadav <76009420+chaman56@users.noreply.github.com>
2023-08-04 22:25:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,37 +4,15 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import { UserAuthContextProvider, useUserAuth } from './context/UserAuthContext';
 import Home from './Home';
-import PackagePage from './components/PackagePage';
-import Main from './components/main';
-import Header from './components/header';
-import Footer from './components/footer';
 
 
 function App() {
-  const { logOut, user } = useUserAuth();
-  const navigate = useNavigate();
-  const handleLogout = async () => {
-    try {
-      await logOut();
-      navigate("/login");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
   return (
         <UserAuthContextProvider>
         <Routes>
           <Route path="/login" element ={<Login/>}/>
           <Route path="/Signup" element ={<Signup/>}/>
-          <Route path="/">
-            <Header user={user} handleLogout={handleLogout} />
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/packages" element ={<PackagePage />}/>
-              <Route path="/packages/:id" element ={<PackagePage />}/>
-            </Routes>
-            <Footer />
-          </Route>
+          <Route path="/" element ={<Home />} />
         </Routes>
         </UserAuthContextProvider>
   );
